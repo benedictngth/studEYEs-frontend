f6168f57044262f9a000ed3a4db9c032d2c42e3c
Ng Tze Han, Benedict <E1406493@u.nus.edu>
2025-07-02 11:02:59 +0800

    add database feature

---
+++ src/components/StatisticsPage.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import supabase from "../utils/supabase";
+interface Stat {
+  id: number;
+  created_at: string;
+  totalStudyDuration: number;
+  totalBreak: number;
+}
+const StatisticsPage = () => {
+  const [stats, setStats] = useState<Stat[]>([]);
+
+  useEffect(() => {
+    async function getStats() {
+      const { data: stats } = await supabase.from("session").select();
+
+      if (stats && stats.length >= 1) {
+        setStats(stats);
+      }
+    }
+    getStats();
+  }, []);
+
+  return (
+    <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Date</th>
+            <th>Total study duration</th>
+            <th>Total break duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stats.map((stat) => (
+            <tr key={stat.id}>
+              <th>{stat.id}</th>
+              <td>
+                {stat.created_at
+                  ? new Date(stat.created_at).toLocaleDateString()
+                  : "N/A"}
+              </td>
+              <td>{stat.totalStudyDuration * 60} mins</td>
+              <td>{stat.totalBreak * 60} mins</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default StatisticsPage;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import StatisticsPage from "./components/StatisticsPage.tsx";
 //import FaceModel from "./components/FaceModel.tsx";
 
 createRoot(document.getElementById("root")!).render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route index element={<App />} />
+        <Route path="/stats" element={<StatisticsPage />} />
         {/* <Route path="/faceDetect" element={<FaceModel />} /> */}
       </Routes>
     </BrowserRouter>
